2d31958114e29a592953d8332336877a17565518
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2023-01-13 19:18:56 +0600

    Added a program for multiple inheritance

---
+++ polygons.py
@@ -0,0 +1,13 @@
+class polygon:
+
+    __length = None
+    __width = None
+    def __init__(self, length, width):
+        self.__length = length
+        self.__width = width
+
+    def get_length(self):
+        return self.__length
+
+    def get_width(self):
+        return self.__width
