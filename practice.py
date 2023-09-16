5ca597ca2145c50bac1bf373633e44238e317612
arfan64shah <arfan64shah@gmail.com>
2023-09-16 11:51:27 +0500

    Added

---
+++ practice.py
@@ -0,0 +1,10 @@
+# define a class
+class number:
+    # initiate a function as constructor
+    def __init__(self, num):
+        
+        self.num = num
+num1 = number(2)
+num2 = number(3)
+
+print(type(num1))
